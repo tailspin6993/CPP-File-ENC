a44f20d69bd9b683fa896dbaba881503350ea5c8
tailspin6993 <fb45f535@protonmail.com>
2024-02-01 18:17:02 -0500

    Add HMAC computation for master key

--- encrypt.cpp
+++ encrypt.cpp
@@ -2,6 +2,16 @@
 #include <iostream>
 #include <fstream>
 #include <sodium.h>
+#include <sodium/utils.h>
+
+void splitFullKey(unsigned char* fullKey, unsigned char* encryptionKey, int encryptionKeyLen, unsigned char* macKey, int macKeyLen) {
+    for (int i = 0; i < encryptionKeyLen; i++) {
+        encryptionKey[i] = fullKey[i];
+    }
+
+    for (int i = 0; i < macKeyLen; i++)
+        macKey[i] = fullKey[i + encryptionKeyLen];
+}
 
 int main() {
     if (sodium_init() > 0) 
@@ -18,10 +28,15 @@ int main() {
     std::cout << "Password (max of 32 chars): ";
     std::cin.getline(password, 32);
 
-    unsigned char key[crypto_stream_xchacha20_KEYBYTES];
+    const std::size_t fullKeyLength = 
+        crypto_stream_xchacha20_KEYBYTES + crypto_generichash_blake2b_KEYBYTES;
+    
+    std::cout << fullKeyLength << std::endl;
+
+    unsigned char fullKey[fullKeyLength];
     int hashStatus = crypto_pwhash(
-        key, 
-        sizeof key,
+        fullKey, 
+        sizeof fullKey,
         password, 
         strlen(password), 
         salt,
@@ -29,6 +44,12 @@ int main() {
         crypto_pwhash_MEMLIMIT_INTERACTIVE,
         crypto_pwhash_ALG_ARGON2ID13
     );
+
+    unsigned char key[crypto_stream_xchacha20_KEYBYTES];
+    unsigned char macKey[crypto_generichash_blake2b_KEYBYTES];
+
+    splitFullKey(fullKey, key, sizeof key, macKey, sizeof macKey);
+    sodium_memzero(fullKey, sizeof fullKey);
 
     // zero-out password ASAP.
     sodium_memzero(password, sizeof password);
@@ -38,11 +59,17 @@ int main() {
         return 1;
     }
 
-    unsigned char masterKey[crypto_stream_xchacha20_KEYBYTES];
+    unsigned char masterFullKey[fullKeyLength];
+    randombytes_buf(masterFullKey, sizeof masterFullKey);
+
+    unsigned char masterEncKey[sizeof key];
+    unsigned char masterMacKey[sizeof macKey];
+
+    splitFullKey(masterFullKey, masterEncKey, sizeof masterEncKey, masterMacKey, sizeof masterMacKey);
+
     unsigned char masterKeyNonce[crypto_stream_xchacha20_NONCEBYTES];
     unsigned char dataNonce[crypto_stream_xchacha20_NONCEBYTES];
 
-    randombytes_buf(masterKey, sizeof masterKey);
     randombytes_buf(masterKeyNonce, sizeof masterKeyNonce);
     randombytes_buf(dataNonce, sizeof dataNonce);
 
@@ -52,35 +79,41 @@ int main() {
     std::ifstream inFile(fileName, std::ios::binary);
     std::ofstream outFile(fileName + "_enc", std::ios::binary);
 
-    crypto_stream_xchacha20_xor(masterKey, masterKey, sizeof masterKey, masterKeyNonce, key);
+    crypto_stream_xchacha20_xor(masterFullKey, masterFullKey, sizeof masterFullKey, masterKeyNonce, key);
+    sodium_memzero(key, sizeof key);
+
+    unsigned char masterFullKeyDigest[crypto_generichash_blake2b_BYTES];
+    crypto_generichash_blake2b(masterFullKeyDigest, sizeof masterFullKeyDigest, masterFullKey, sizeof masterFullKey, macKey, sizeof macKey);
+
+    sodium_memzero(macKey, sizeof macKey);
+
     outFile.write(reinterpret_cast<char*>(&dataNonce), sizeof dataNonce);
     outFile.write(reinterpret_cast<char*>(&masterKeyNonce), sizeof masterKeyNonce);
     outFile.write(reinterpret_cast<char*>(&salt), sizeof salt);
-    outFile.write(reinterpret_cast<char*>(&masterKey), sizeof masterKey);
+    outFile.write(reinterpret_cast<char*>(&masterFullKeyDigest), sizeof masterFullKeyDigest);
+    outFile.write(reinterpret_cast<char*>(&masterFullKey), sizeof masterFullKey);
 
-    // decrypt master key for file encryption
-    crypto_stream_xchacha20_xor(masterKey, masterKey, sizeof masterKey, masterKeyNonce, key);
+    sodium_memzero(salt, sizeof salt);
+    sodium_memzero(masterFullKey, sizeof masterFullKey);
+    sodium_memzero(masterKeyNonce, sizeof masterKeyNonce);
+    sodium_memzero(masterFullKeyDigest, sizeof masterFullKeyDigest);
 
     while (inFile) {
         inFile.read(reinterpret_cast<char*>(&buff), CHUNK_SIZE);
         std::streamsize bytesRead = inFile.gcount();
 
         if (bytesRead > 0) {
-            crypto_stream_xchacha20_xor(buff, buff, sizeof buff, dataNonce, masterKey);
+            crypto_stream_xchacha20_xor(buff, buff, sizeof buff, dataNonce, masterEncKey);
             outFile.write(reinterpret_cast<char*>(&buff), bytesRead);
 
             sodium_memzero(buff, sizeof buff);
         }
     }
 
-    sodium_memzero(key, sizeof key);
-    sodium_memzero(masterKey, sizeof masterKey);
+    sodium_memzero(masterEncKey, sizeof masterEncKey);
+    sodium_memzero(masterMacKey, sizeof masterMacKey);
+    sodium_memzero(dataNonce, sizeof dataNonce);
 
     inFile.close();
     outFile.close();
-
-    sodium_memzero(masterKeyNonce, sizeof masterKeyNonce);
-    sodium_memzero(dataNonce, sizeof dataNonce);
-    sodium_memzero(salt, sizeof salt);
-    sodium_memzero(buff, sizeof buff);
 }
