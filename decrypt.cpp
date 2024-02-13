521a39ac3f0e8739104687c46d1af65cbc406f38
tailspin6993 <fb45f535@protonmail.com>
2024-02-13 17:16:42 -0500

    add HMAC verification of chunks when decrypting

--- decrypt.cpp
+++ decrypt.cpp
@@ -2,10 +2,29 @@
 #include <iostream>
 #include <fstream>
 #include <sodium.h>
+#include <sodium/crypto_generichash.h>
+#include <sodium/crypto_generichash_blake2b.h>
+#include <sodium/crypto_stream_xchacha20.h>
+#include <sodium/utils.h>
+
+void splitFullKey(unsigned char* fullKey, unsigned char* encryptionKey, int encryptionKeyLen, unsigned char* macKey, int macKeyLen) {
+    for (int i = 0; i < encryptionKeyLen; i++) {
+        encryptionKey[i] = fullKey[i];
+    }
+
+    for (int i = 0; i < macKeyLen; i++)
+        macKey[i] = fullKey[i + encryptionKeyLen];
+}
 
 int main() {
     if (sodium_init() > 0) 
         std::cout << "Libsodium failed to initialize." << std::endl;
+
+    const std::size_t fullKeyLength = 
+        crypto_stream_xchacha20_KEYBYTES + crypto_generichash_blake2b_KEYBYTES;
+    
+    const std::size_t CHUNK_DIGEST_SIZE = 32;
+    const std::size_t CHUNK_SIZE = 1024;
         
     std::string fileName;
     std::cout << "File to decrypt: ";
@@ -15,16 +34,15 @@ int main() {
     std::cout << "Password (max of 32 chars): ";
     std::cin.getline(password, 32);
 
-    const std::size_t CHUNK_SIZE = 1024;
     unsigned char buff[CHUNK_SIZE];
 
     std::ifstream inFile(fileName, std::ios::binary);
-    std::ofstream outFile(fileName + "_dec", std::ios::binary);
 
     unsigned char masterKeyNonce[crypto_stream_xchacha20_NONCEBYTES];
     unsigned char dataNonce[crypto_stream_xchacha20_NONCEBYTES];
     unsigned char salt[crypto_pwhash_argon2id_SALTBYTES];
-    unsigned char masterKey[crypto_stream_xchacha20_KEYBYTES];
+    unsigned char masterKeyDigest[crypto_generichash_blake2b_BYTES];
+    unsigned char masterKey[fullKeyLength];
 
     int seekPos = 0;
 
@@ -41,12 +59,16 @@ int main() {
         seekPos += sizeof salt;
         inFile.seekg(seekPos);
 
+        inFile.read(reinterpret_cast<char*>(masterKeyDigest), sizeof masterKeyDigest);
+        seekPos += sizeof masterKeyDigest;
+        inFile.seekg(seekPos);
+
         inFile.read(reinterpret_cast<char*>(masterKey), sizeof masterKey);
         seekPos += sizeof masterKey;
         inFile.seekg(seekPos);
     }
     
-    unsigned char key[crypto_stream_xchacha20_KEYBYTES];
+    unsigned char key[fullKeyLength];
     int hashStatus = crypto_pwhash(
         key, 
         sizeof key,
@@ -58,26 +80,61 @@ int main() {
         crypto_pwhash_ALG_ARGON2ID13
     );
 
-    // zero-out password ASAP.
-    sodium_memzero(password, sizeof password);
 
     if (hashStatus != 0) {
         std::cout << "Key derivation failed." << std::endl;
         return 1;
     }
 
-    crypto_stream_xchacha20_xor(masterKey, masterKey, sizeof masterKey, masterKeyNonce, key);
+    unsigned char userEncKey[crypto_stream_xchacha20_KEYBYTES];
+    unsigned char userMacKey[crypto_generichash_KEYBYTES];
+
+    splitFullKey(key, userEncKey, sizeof userEncKey, userMacKey, sizeof userMacKey);
+
+    unsigned char computedMasterKeyDigest[crypto_generichash_blake2b_BYTES];
+    crypto_generichash_blake2b(computedMasterKeyDigest, sizeof computedMasterKeyDigest, masterKey, sizeof masterKey, userMacKey, sizeof userMacKey);
+
+    if (sodium_memcmp(computedMasterKeyDigest, masterKeyDigest, sizeof computedMasterKeyDigest) != 0) {
+        std::cout << "Incorrect password." << std::endl;
+        return 1;
+    }
+
+    crypto_stream_xchacha20_xor(masterKey, masterKey, sizeof masterKey, masterKeyNonce, userEncKey);
+
+    unsigned char masterEncKey[sizeof userEncKey];
+    unsigned char masterMacKey[sizeof userMacKey];
+
+    splitFullKey(masterKey, masterEncKey, sizeof masterEncKey, masterMacKey, sizeof masterMacKey);
+
+    // zero-out password ASAP.
+    sodium_memzero(password, sizeof password);
     sodium_memzero(key, sizeof key);
+    sodium_memzero(userEncKey, sizeof userEncKey);
+    sodium_memzero(userMacKey, sizeof userMacKey);
+
+    std::ofstream outFile(fileName + "_dec", std::ios::binary);
 
     while (inFile) {
+        unsigned char byteBlockDigest[CHUNK_DIGEST_SIZE];
+        inFile.read(reinterpret_cast<char*>(&byteBlockDigest), CHUNK_DIGEST_SIZE);
         inFile.read(reinterpret_cast<char*>(&buff), CHUNK_SIZE);
         std::streamsize bytesRead = inFile.gcount();
 
         if (bytesRead > 0) {
+            unsigned char computedByteBlockDigest[CHUNK_DIGEST_SIZE];
+            crypto_generichash_blake2b(computedByteBlockDigest, sizeof computedByteBlockDigest, buff, bytesRead, masterMacKey, sizeof masterMacKey);
+
+            if (sodium_memcmp(byteBlockDigest, computedByteBlockDigest, sizeof computedByteBlockDigest) != 0) {
+                std::cout << "MAC verification failed for byte block." << std::endl;
+                return 1;
+            }
+
             crypto_stream_xchacha20_xor(buff, buff, sizeof buff, dataNonce, masterKey);
             outFile.write(reinterpret_cast<char*>(&buff), bytesRead);
 
             sodium_memzero(buff, sizeof buff);
+            sodium_memzero(byteBlockDigest, sizeof byteBlockDigest);
+            sodium_memzero(computedByteBlockDigest, sizeof computedByteBlockDigest);
         }
     }
 
@@ -90,4 +147,6 @@ int main() {
     sodium_memzero(dataNonce, sizeof dataNonce);
     sodium_memzero(salt, sizeof salt);
     sodium_memzero(buff, sizeof buff);
+
+    std::cout << "Successfully decrypted." << std::endl;
 }
